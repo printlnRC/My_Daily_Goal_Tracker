dc5dd131666ea26b2c4a830fb0007d1d5237a25b
printlnrc <plush.huit@gmail.com>
2026-02-04 11:49:44 +0100

    feat: toast ajout de goal

--- My_Daily_Goal_Tracker/app/src/App.tsx
+++ My_Daily_Goal_Tracker/app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import GoalForm, { Task } from "./components/GoalForm";
+import { Toaster } from 'sonner';
 
 function App() {
   const [goals, setGoals] = useState<Task[]>([]);
@@ -10,18 +11,21 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 p-5">
-      <div className="w-12/13 my-10">
-        {/* On appelle le composant et on lui passe la fonction de gestion */}
-        <GoalForm onAddGoal={handleAddGoal} />
-        
-        {/* Ici tu pourras plus tard mapper tes goals pour les afficher */}
-        <div className="mt-5">
-          {goals.map((g) => (
-            <div key={g.id} className="p-2 border-b">
-               {g.text} - <span className="badge">{g.priority}</span>
-            </div>
-          ))}
+    <div className="min-h-screen bg-base-100">
+      <Toaster richColors position="top-right" />
+      <div className="flex flex-col items-center gap-4 p-5">
+        <div className="w-12/13 my-10">
+          {/* On appelle le composant et on lui passe la fonction de gestion */}
+          <GoalForm onAddGoal={handleAddGoal} />
+
+          {/* Ici tu pourras plus tard mapper tes goals pour les afficher */}
+          <div className="mt-5">
+            {goals.map((g) => (
+              <div key={g.id} className="p-2 border-b">
+                {g.text} - <span className="badge">{g.priority}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
